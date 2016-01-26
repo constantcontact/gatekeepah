f735b17dedb229c406885f82d4e9040ec9688b79
Ryan Davis <rdavis@constantcontact.com>
2016-01-26 12:50:23 -0500

    Fixing Tests

--- src/test/java/com/constantcontact/plugins/GateKeepah/GateKeepahBuilderPropertiesTest.java
+++ src/test/java/com/constantcontact/plugins/GateKeepah/GateKeepahBuilderPropertiesTest.java
@@ -1,5 +1,6 @@
 package com.constantcontact.plugins.GateKeepah;
 
+import java.io.File;
 import java.net.URL;
 import java.util.Properties;
 
@@ -10,10 +11,12 @@ public class GateKeepahBuilderPropertiesTest {
 
 	@Test
 	public void testReadPropertiesHappy() throws Exception {
-		URL url = getClass().getResource("testconfig.properties");
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+		URL url = classloader.getResource("testconfig.properties");
 
 		GateKeepahBuilder gateKeepahBuilder = new GateKeepahBuilder(url.getPath(), "");
-		final String filePath = url.getPath().replace("/testconfig.properties", "");
+		final String filePath = url.getPath().replace(File.separator + "testconfig.properties", "");
 		Properties props = gateKeepahBuilder.readPropertiesFile(filePath, "testconfig.properties", "");
 
 		Assert.assertEquals("test", props.getProperty("test"));
@@ -22,10 +25,11 @@ public class GateKeepahBuilderPropertiesTest {
 
 	@Test
 	public void testReadPropertiesHappyAdded() throws Exception {
-		URL url = getClass().getResource("testconfig.properties");
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		URL url = classloader.getResource("testconfig.properties");
 
 		GateKeepahBuilder gateKeepahBuilder = new GateKeepahBuilder(url.getPath(), "test3=test3");
-		final String filePath = url.getPath().replace("/testconfig.properties", "");
+		final String filePath = url.getPath().replace(File.separator + "testconfig.properties", "");
 		Properties props = gateKeepahBuilder.readPropertiesFile(filePath, "testconfig.properties", "test3=test3");
 
 		Assert.assertEquals("test3", props.getProperty("test3"));
@@ -33,10 +37,11 @@ public class GateKeepahBuilderPropertiesTest {
 
 	@Test
 	public void testReadPropertiesHappyOnlyAdditional() throws Exception {
-		URL url = getClass().getResource("testconfig.properties");
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		URL url = classloader.getResource("testconfig.properties");
 
 		GateKeepahBuilder gateKeepahBuilder = new GateKeepahBuilder(null, "test3=test3");
-		final String filePath = url.getPath().replace("/testconfig.properties", "");
+		final String filePath = url.getPath().replace(File.separator + "testconfig.properties", "");
 		Properties props = gateKeepahBuilder.readPropertiesFile(filePath, null, "test3=test3");
 
 		Assert.assertEquals("test3", props.getProperty("test3"));
@@ -44,10 +49,11 @@ public class GateKeepahBuilderPropertiesTest {
 
 	@Test
 	public void testReadPropertiesFileMissingValue() throws Exception {
-		URL url = getClass().getResource("testconfigMissingValue.properties");
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		URL url = classloader.getResource("testconfigMissingValue.properties");
 
 		GateKeepahBuilder gateKeepahBuilder = new GateKeepahBuilder(url.getPath(), "");
-		final String filePath = url.getPath().replace("/testconfigMissingValue.properties", "");
+		final String filePath = url.getPath().replace(File.separator + "testconfigMissingValue.properties", "");
 		Properties props = gateKeepahBuilder.readPropertiesFile(filePath, "testconfigMissingValue.properties", "");
 		Assert.assertEquals(null, props.get("test="));
 
@@ -63,7 +69,7 @@ public class GateKeepahBuilderPropertiesTest {
 		}
 
 		GateKeepahBuilder gateKeepahBuilder = new GateKeepahBuilder(url.getPath(), "");
-		final String filePath = url.getPath().replace("/testconfigEmpty.properties", "");
+		final String filePath = url.getPath().replace(File.separator + "testconfigEmpty.properties", "");
 		Properties props = gateKeepahBuilder.readPropertiesFile(filePath, "testconfigEmpty.properties", "");
 		Assert.assertEquals(0, props.size());
 
@@ -71,10 +77,11 @@ public class GateKeepahBuilderPropertiesTest {
 
 	@Test
 	public void testReadPropertiesAdditionalProperties() throws Exception {
-		URL url = getClass().getResource("testconfig.properties");
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		URL url = classloader.getResource("testconfig.properties");
 
 		GateKeepahBuilder gateKeepahBuilder = new GateKeepahBuilder(url.getPath(), "");
-		final String filePath = url.getPath().replace("/testconfig.properties", "");
+		final String filePath = url.getPath().replace(File.separator + "testconfig.properties", "");
 		try {
 			gateKeepahBuilder.readPropertiesFile(filePath, "testconfig.properties", "test4=\n");
 			Assert.fail("Should have thrown an exception");
@@ -86,10 +93,11 @@ public class GateKeepahBuilderPropertiesTest {
 
 	@Test
 	public void testReadPropertiesHappyOnlyAdditionalMissingValue() throws Exception {
-		URL url = getClass().getResource("testconfig.properties");
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		URL url = classloader.getResource("testconfig.properties");
 
 		GateKeepahBuilder gateKeepahBuilder = new GateKeepahBuilder(null, "test3=test3");
-		final String filePath = url.getPath().replace("/testconfig.properties", "");
+		final String filePath = url.getPath().replace(File.separator + "testconfig.properties", "");
 		try {
 			gateKeepahBuilder.readPropertiesFile(filePath, null, "test4=\n");
 			Assert.fail("Should have thrown en exception");
